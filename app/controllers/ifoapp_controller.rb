12c40cd079f4841e69deab965ecf6c6646d593f2
Scott Japs <scott.japs@gmail.com>
2012-11-29 11:05:15 -0600

    Update app/controllers/ifoapp_controller.rb

--- app/controllers/ifoapp_controller.rb
+++ app/controllers/ifoapp_controller.rb
@@ -93,7 +93,7 @@ class IfoappController < ApplicationController
   end
   
   def staff_login
-    if "the_pass" == params[:password]  #this is the password, it needs to be changed
+    if "Vol2013" == params[:password]  #this is the password, it needs to be changed
       redirect_to :action => "viewall" and return
     else
       flash[:notice] = "wrong password"
